eebc70aaf34cf2b38b5733c179168d31ecb1c445
The Skye Dino <yourtransdino@gmail.com>
2025-11-28 21:22:19 -0500

    Update Phys-Calculator.py

--- Phys-Calculator.py
+++ Phys-Calculator.py
@@ -12,4 +12,8 @@ Values = {}
 for var in Vars_Obtained:
     Values[var] = float(input(f"What is the value of {var}: ")) #Obtains the value of each variable that the user already has
 
-print(Values)
+if Var_Needed.lower() == "d" and "v1" in Values and "t" in Values:
+    answ = Values["v1"] * Values["t"] + (0.5 * -9.8) * (Values["t"] * Values["t"])
+
+
+print(f"{Var_Needed} is {answ}")
